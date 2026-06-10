ff406c3141a2e712634da4afc5429bf904ca9c2f
halinh-vu <fuertoram@gmail.com>
2026-06-10 18:33:20 +0200

    Array Dozent in der Main erstellt

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Dozent dozent = new Dozent("Biologie", 123, true);
+       Person[] personen = new Person[4];
+       personen[0] = new Dozent("Biologie", 1, false);
+       personen[1] = new Student(9, 87);
+       personen[2] = new Dozent("Chemie", 2, true);
+       personen[3] = new Student(10, 83);
     }
 }
