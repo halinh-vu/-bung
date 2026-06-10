f64440053b181e19f60fff279685ccaad77206dc
kristafleurisson-sketch <kristafleurisson@gmail.com>
2026-06-10 18:46:08 +0200

    Getters und Setters

--- Person.java
+++ Person.java
@@ -1,5 +1,26 @@
 public class Person {
     private String nachname;
     private String vorname;
-    
+
+    public Person(String nachname, String vorname){
+        this.nachname = nachname;
+        this.vorname = vorname;
+    }
+
+    public void setNachname(String nachname){
+        this.nachname = nachname;
+    }
+
+    public void setVorname(String vorname){
+       this.vorname = vorname;
+    }
+
+    public String getNachname(){
+          return nachname;
+    }
+
+    public String getVorname(){
+        return vorname;
+    }
+
 }
