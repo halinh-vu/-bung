17464c6c3e19b6061ee5e880628502e09701a439
halinh-vu <fuertoram@gmail.com>
2026-06-10 18:19:53 +0200

    Konstruktor erstellt, Objekt Dozent in Main erstellt und initialisiert. <3

--- Dozent.java
+++ Dozent.java
@@ -1,5 +1,11 @@
 public class Dozent {
     private String fachgebiet;
     private int nummer;
-    
+    private boolean istGastdozent;
+
+    public Dozent(String fachgebiet, int nummer, boolean istGastdozent) {
+        this.fachgebiet = fachgebiet;
+        this.nummer = nummer;
+        this.istGastdozent = istGastdozent;
+    }
 }
